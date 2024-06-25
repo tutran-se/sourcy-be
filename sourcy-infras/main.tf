2e86cac2e35271e4a1ecc0a83ece9b516b612e4d
tutran-se <trandinhtu.dev@gmail.com>
2024-06-25 14:09:21 +0700

    scale ecs task to 5 and add load test

--- sourcy-infras/main.tf
+++ sourcy-infras/main.tf
@@ -191,7 +191,7 @@ resource "aws_ecs_service" "app" {
   name            = "app-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 1
+  desired_count   = 5
   launch_type     = "FARGATE"
   network_configuration {
     subnets          = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
